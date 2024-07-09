8c1bcfe2a5798d6d18f528a0d03f4d098687dd01
Luiz Aguiar <104395076+LuizGLAguiar@users.noreply.github.com>
2024-07-09 15:03:36 -0300

    Update main_script.sh

--- sources/main_script.sh
+++ sources/main_script.sh
@@ -2,6 +2,6 @@
 
 for i in {1..3}; do fswebcam -r 1280x720 /home/aguiar/Desktop/SOE_Projeto/photos/pics_taken/foto_$i.jpg; done
 
-/home/aguiar/Desktop/SOE_Projeto/sources/people_detection.out /home/aguiar/Desktop/SOE_Projeto/photos/pics_taken/*.jpg
+/home/pi/Desktop/SOE/SOE_Projeto/sources/people_detection.out /home/pi/Desktop/SOE/SOE_Projeto/photos/pics_taken/*.jpg
 
-/home/aguiar/Desktop/SOE_Projeto/sources/grafico.out 
+/home/pi/Desktop/SOE/SOE_Projeto/sources/grafico.out 
