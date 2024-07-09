9f94bb17a94f23c4ff14f2a9911d7b761ec5b02c
Luiz Aguiar <luizleaoaguiar@gmail.com>
2024-07-09 20:17:23 -0300

    Update main_script.sh

--- sources/main_script.sh
+++ sources/main_script.sh
@@ -1,6 +1,6 @@
 !/bin/bash
 
-for i in {1..3}; do fswebcam -r 1280x720 /home/aguiar/Desktop/SOE_Projeto/photos/pics_taken/foto_$i.jpg; done
+for i in {1..3}; do fswebcam -r 1280x720 /home/pi/Desktop/SOE/SOE_Projeto/photos/pics_taken/foto_$i.jpg; done
 
 /home/pi/Desktop/SOE/SOE_Projeto/sources/people_detection.out /home/pi/Desktop/SOE/SOE_Projeto/photos/pics_taken/*.jpg
 
